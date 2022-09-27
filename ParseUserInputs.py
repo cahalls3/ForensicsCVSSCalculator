ff8ea52037dd73ef8ca271a23b2350494ac9924f
Roman Vish <romanvish7@gmail.com>
2022-09-27 14:18:08 -0600

    Update funcs

--- ParseUserInputs.py
+++ ParseUserInputs.py
@@ -1,2 +1,29 @@
 
-def convertUI2
+import math
+
+
+def convertUI2MathScore(val):
+    mathScore = -1
+
+    if val == 1:
+        mathScore = 3
+    elif val == 2:
+        mathScore = 6
+    elif val == 3:
+        mathScore = 9
+    else: 
+        return 0
+
+    return mathScore
+
+def binaryResponse2MathScore(val):
+    mathScore = -1
+
+    if val == 1:
+        mathScore = 0
+    elif val == 2:
+        mathScore = 10
+    else:
+        mathScore = 5
+    
+    return mathScore
---
+++ UserInputs.py
@@ -0,0 +1,8 @@
+
+from urllib import response
+
+
+def getUserInput(str):
+    response = input(str)
+
+    return int(response)
