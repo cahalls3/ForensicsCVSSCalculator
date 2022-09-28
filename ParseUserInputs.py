be80fa3c36a6b672dc524e6cc7e6f8580fa6a35a
cahalls3 <carsonahalls3@gmail.com>
2022-09-28 14:27:01 -0600

    Added Equations in the comments

--- ParseUserInputs.py
+++ ParseUserInputs.py
@@ -27,3 +27,25 @@ def binaryResponse2MathScore(val):
         mathScore = 0
     
     return mathScore
+
+
+#THE COMMENTS BELOW ARE THE EQUATIONS THAT THE WEBSITE USES TO CALCULATE RISK.
+
+# The Base Score is a function of the Impact and Exploitability sub score equations. Where the Base score is defined as,
+
+# If (Impact sub score <= 0)     0 else,
+# Scope Unchanged4                 𝑅𝑜𝑢𝑛𝑑𝑢𝑝(𝑀𝑖𝑛𝑖𝑚𝑢𝑚[(𝐼𝑚𝑝𝑎𝑐𝑡 + 𝐸𝑥𝑝𝑙𝑜𝑖𝑡𝑎𝑏𝑖𝑙𝑖𝑡𝑦), 10])
+# Scope Changed                      𝑅𝑜𝑢𝑛𝑑𝑢𝑝(𝑀𝑖𝑛𝑖𝑚𝑢𝑚[1.08 × (𝐼𝑚𝑝𝑎𝑐𝑡 + 𝐸𝑥𝑝𝑙𝑜𝑖𝑡𝑎𝑏𝑖𝑙𝑖𝑡𝑦), 10])
+
+# and the Impact sub score (ISC) is defined as,
+
+# Scope Unchanged 6.42 × 𝐼𝑆𝐶Base
+# Scope Changed 7.52 × [𝐼𝑆𝐶𝐵𝑎𝑠𝑒 − 0.029] − 3.25 × [𝐼𝑆𝐶𝐵𝑎𝑠𝑒 − 0.02]15
+
+# Where,
+
+# 𝐼𝑆𝐶𝐵𝑎𝑠𝑒 = 1 − [(1 − 𝐼𝑚𝑝𝑎𝑐𝑡𝐶𝑜𝑛𝑓) × (1 − 𝐼𝑚𝑝𝑎𝑐𝑡𝐼𝑛𝑡𝑒𝑔) × (1 − 𝐼𝑚𝑝𝑎𝑐𝑡𝐴𝑣𝑎𝑖𝑙)]
+
+# And the Exploitability sub score is,
+
+# 8.22 × 𝐴𝑡𝑡𝑎𝑐𝑘𝑉𝑒𝑐𝑡𝑜𝑟 × 𝐴𝑡𝑡𝑎𝑐𝑘𝐶𝑜𝑚𝑝𝑙𝑒𝑥𝑖𝑡𝑦 × 𝑃𝑟𝑖𝑣𝑖𝑙𝑒𝑔𝑒𝑅𝑒𝑞𝑢𝑖𝑟𝑒𝑑 × 𝑈𝑠𝑒𝑟𝐼𝑛𝑡𝑒𝑟𝑎𝑐𝑡𝑖𝑜𝑛
