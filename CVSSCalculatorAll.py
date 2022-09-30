af340a18a41d580b3f231701215febd21adeef73
Roman Vish <romanvish7@gmail.com>
2022-09-30 12:55:18 -0600

    Remove dead code

--- CVSSCalculatorAll.py
+++ CVSSCalculatorAll.py
@@ -2,32 +2,6 @@ import string
 from urllib import response
 import math
 
-def convertUI2MathScore(val):
-    mathScore = -1
-
-    if val == 1:
-        mathScore = 3.33
-    elif val == 2:
-        mathScore = 6.66
-    elif val == 3:
-        mathScore = 9.99
-    else: 
-        return 0
-
-    return mathScore
-
-def binaryResponse2MathScore(val):
-    mathScore = -1
-
-    if val == 1:
-        mathScore = 0
-    elif val == 2:
-        mathScore = 10
-    else:
-        mathScore = 0
-    
-    return mathScore
-
 def scoreCalc():
     exploitabilityCoefficient = 8.22
     scopeCoefficient = 1.08
